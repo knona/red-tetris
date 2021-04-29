a5344862bbd1d6fd3760146ad758c40b9347bc81
krambono <krambono@student.42lyon.fr>
2021-04-29 14:02:17 +0200

    fix eslint path

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './client/tsconfig.json',
     sourceType: 'module'
   },
   settings: {
